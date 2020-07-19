ec7cc454018abce3421feea0d28157207c1bdf08
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2020-07-19 11:11:02 +0900

    style: try linaria

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,34 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import { Page } from '@zeit-ui/react'
+import { Page, useTheme } from '@zeit-ui/react'
+import { styled } from 'linaria/react'
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+type PageHeaderProps = {
+  backgroundColor: string
+}
+
+const PageHeader = styled(Page.Header)<PageHeaderProps>`
+  background-color: ${props => props.backgroundColor};
+`
+
+const Layout = ({ children, title = 'This is the default title' }: Props) => {
+  const { palette } = useTheme()
+
+  return (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <Page size="small">
-      <Page.Header>
+    <Page size="medium">
+      <PageHeader backgroundColor={palette.accents_2}>
         <nav>
           <Link href="/">
             <a>Home</a>
@@ -31,7 +43,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
           </Link>{' '}
           | <a href="/api/users">Users API</a>
         </nav>
-      </Page.Header>
+      </PageHeader>
       <Page.Content>
         {children}
       </Page.Content>
@@ -40,28 +52,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         <span>I'm here to stay (Footer)</span>
       </Page.Footer>
     </Page>
-    {/* <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer> */}
   </div>
-)
+)}
 
 export default Layout
